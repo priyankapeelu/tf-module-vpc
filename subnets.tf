1e76c743d1b584a744893a3466c169c9cadaf9b0
priyankapeelu <priyankapeelu@icloud.com>
2022-04-22 18:18:09 +0530

    terraform variablescd tt

--- subnets.tf
+++ subnets.tf
@@ -1,7 +1,7 @@
 resource "aws_subnet" "main" {
-  count        = length(var.SUBNET_CIDR)
-  vpc_id       = {aws_vpc.main.id}
-  cidr_block   = element(var.SUBNET_CIDR, count.index)
+  count             =       length(var.SUBNET_CIDR)
+  vpc_id            =          {aws_vpc.main.id}
+  cidr_block        = element(var.SUBNET_CIDR, count.index)
   availability_zone = element(var.AZ, count.index)
 
 
